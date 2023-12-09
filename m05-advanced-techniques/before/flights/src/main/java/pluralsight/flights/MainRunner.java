2210efc6043c2981b9483b710245d4791118b05a
Dan Geabunea <dangeabunea@hotmail.com>
2023-12-09 08:10:23 +0200

    Upgraded to version 3.1.5 demo 5;

--- m05-advanced-techniques/before/flights/src/main/java/pluralsight/flights/MainRunner.java
+++ m05-advanced-techniques/before/flights/src/main/java/pluralsight/flights/MainRunner.java
@@ -3,6 +3,11 @@ package pluralsight.flights;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pluralsight.flights.dal.FlightPlanRepository;
+import pluralsight.flights.domain.AircraftFactory;
+import pluralsight.flights.domain.FlightPlan;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /*
 This component will be executed by Spring Framework immediately after application
@@ -10,14 +15,22 @@ bootstrap
  */
 @Component
 public class MainRunner implements CommandLineRunner {
-    private FlightPlanRepository repository;
+    private FlightPlanRepository flightPlanRepository;
 
-    public MainRunner(FlightPlanRepository repository) {
-        this.repository = repository;
+    public MainRunner(FlightPlanRepository flightPlanRepository) {
+        this.flightPlanRepository = flightPlanRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-
+        flightPlanRepository.insert(new FlightPlan(
+                "Vienna",
+                "Bucharest",
+                LocalDateTime.of(2023, 5, 1, 20, 15),
+                60 * 70,
+                List.of("Austria", "Hngary", "Romania"),
+                true,
+                AircraftFactory.buildEmbraerE175()
+        ));
     }
 }
